2e282c35df3d83bbda9ca718cbde711c4fecfe6b
CrispyCabot <18431358+CrispyCabot@users.noreply.github.com>
2019-02-20 12:47:53 -0500

    stuff

--- SnakeVsRoyale.py
+++ SnakeVsRoyale.py
@@ -4,6 +4,7 @@ import os
 import math
 from random import randint
 from pygame.locals import *
+import time
 
 pygame.init()
 
@@ -19,59 +20,80 @@ font = pygame.font.Font(os.path.join(PATH,'font.ttf'), 36)
 
 clock = pygame.time.Clock()
 
+length = 1
+
 class Snake:
+    global length
     def __init__(self, pos, dir, name):
         self.bod = [] #Lists [[x,y], [x,y]...]
         self.dir = dir #String 'direction'
-        self.pos = pos #List [x,y]
+        self.pos = pos.copy() #List [x,y]
         self.name = name #Name 'Chris'
         self.length = 1
+        self.alive = True
     def getInfo(self):
         return [self.bod, self.dir, self.pos]
     def setDir(self, info):
         self.dir = getattr(playerUpdates, self.name)(self.pos, self.bod, self.dir, info)
     def update(self, win):
-        if self.dir == 'right':
-            self.pos[0]+=size
-        elif self.dir == 'left':
-            self.pos[0]-=size
-        elif self.dir == 'up':
-            self.pos[1]-=size
-        elif self.dir == 'down':
-            self.pos[1]+=size
-        else:
-            print("No direction set", self.name)
-        self.bod.append([self.pos[0], self.pos[1]])
-        if len(self.bod) > self.length:
-            self.bod.pop(0)
+        if self.alive:
+            if self.dir == 'right':
+                self.pos[0]+=size
+            elif self.dir == 'left':
+                self.pos[0]-=size
+            elif self.dir == 'up':
+                self.pos[1]-=size
+            elif self.dir == 'down':
+                self.pos[1]+=size
+            else:
+                print("No direction set", self.name)
+            self.bod.append([self.pos[0], self.pos[1]])
+            if len(self.bod) > length: #length is global, self.length is the class variable
+                self.bod.pop(0)
         for i in self.bod:
             pygame.draw.rect(win, (0,255,0), pygame.Rect(i[0], i[1], size, size))
-        #Check for hit
 
 def main():
-    players = ['Chris']
+    global length
+    players = ['Chris', 'Cary']
     snakeList = []
     info = []
 
     dirs = ['right', 'left', 'up', 'down']
     for i in players:
-        x = randint(0,(width/size))*size
-        y = randint(0,(height/size))*size
-        snake = Snake([x, y], dirs[randint(0,3)], i)
+        snake = Snake([randint(0,(width/size)-1)*size, randint(0,(height/size)-1)*size], dirs[randint(0,3)], i)
         snakeList.append(snake)
 
     end = True
     playing = True
+    tStart = time.time()
     while playing:
         clock.tick(50)
         for event in pygame.event.get():
             if event.type == QUIT:
                 playing = False
+                end = False
 
         info = []
         for i in snakeList:
             i.setDir(info)
             info.append(i.getInfo())
+        #Check hit
+        for i in snakeList:
+            if i.alive:
+                loc = i.getInfo()[2]
+                for x in info:
+                    if loc in x[0]:
+                        if not(loc==x[2]): #Makes sure it isn't current position
+                            i.alive = False
+                            print(i.name, 'is out (Hit snake)')
+                if loc[0] < 0 or loc[0]>=width or loc[1]<0 or loc[1]>=height:
+                    i.alive = False
+                    print(i.name, 'is out (Border)')
+
+        if (time.time()-tStart) > 1:
+            length += 1
+            tStart = time.time()
 
         #Draw everything
         pygame.draw.rect(win, (0,0,0), pygame.Rect(0,0,width,height))
@@ -89,37 +111,35 @@ def main():
         for i in keys:
             if i:
                 end = False
-        #Draw everything - identical to above
+        #Draw everything
         pygame.draw.rect(win, (0,0,0), pygame.Rect(0,0,width,height)) #Background
 
-        for i in bod1:
-            pygame.draw.rect(win, (255,0,0), pygame.Rect(i[0],i[1],size,size))
-        pygame.draw.rect(win, (255,0,0), pygame.Rect(x1,y1,size,size))
-        for i in bod2:
-            pygame.draw.rect(win, (0,255,0), pygame.Rect(i[0],i[1],size,size))
-        pygame.draw.rect(win, (0,255,0), pygame.Rect(x2,y2,size,size))
 
-        if p1lose:
-            pygame.draw.rect(win, (255,255,0), pygame.Rect(x1,y1,size,size))
-        if p2lose:
-            pygame.draw.rect(win, (255,255,0), pygame.Rect(x2,y2,size,size))
-        if p1lose:
-            pygame.draw.rect(win, (255,255,0), pygame.Rect(x1,y1,size,size))
-            text = font.render("Player 2 Wins (Green)", True, (255,255,0))
-            loc = text.get_rect()
-            loc.center = (width/2, height/2)
-            win.blit(text, loc)
-        if p2lose:
-            pygame.draw.rect(win, (255,255,0), pygame.Rect(x2,y2,size,size))
-            text = font.render("Player 1 Wins (Red)", True, (255,255,0))
-            loc = text.get_rect()
-            loc.center = (width/2, height/2)
-            win.blit(text, loc)
         pygame.display.update()
     pygame.quit()
 
 class playerUpdates:
     def Chris(pos, bod, dir, info):
+        newX, newY = pos[0], pos[1]
+        if dir == 'right':
+            newX += size
+        if dir == 'left':
+            newX -= size
+        if dir == 'up':
+            newY -= size
+        if dir == 'down':
+            newY += size
+
+        if newX >= width:
+            return 'up'
+        if newX < 0:
+            return 'down'
+        if newY >= height:
+            return 'right'
+        if newY < 0:
+            return 'left'
+        return dir
+    def Cary(pos, bod, dir, info):
         newX, newY = pos[0], pos[1]
         if dir == 'right':
             newX += size
